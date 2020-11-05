0f9822f9416a7c7c85565b6bee3f045a51a608db
Nicolas Eisemann <71433649+NicholasEisemann@users.noreply.github.com>
2020-11-05 15:08:07 +0300

    rename main

--- main(eng).py
+++ main(eng).py
@@ -1,14 +1,14 @@
 import sqlite3
 
-""" connecting to SQL """
+""" Подключаемся к SQL """
 
 conn = sqlite3.connect("orders.db")
 cur = conn.cursor()
 
-""" creating database """
+""" Создаем базу данных """
 
 cur.execute("""CREATE TABLE IF NOT EXISTS notes(
-    title TEXT,
+    teg TEXT,
     note TEXT);
 """)
 conn.commit()
@@ -16,10 +16,10 @@ conn.commit()
 
 def input_user():
     global command
-    command = input("\033[34mPlease, input command: \033[0m")
+    command = input("\033[34mПожалуйста, введите команду: \033[0m")
 
 
-""" A function that returns the result of the command input to the user  """
+""" Функция которая возвращает пользователю итог ввода команды """
 
 
 def command_user():
@@ -30,30 +30,30 @@ def command_user():
     elif command.lower() == "help":
         help_user()
     elif command.lower() == "exit":
-        print("\033[36m\033[1mGoodBye!\033[0m")
+        print("\033[36m\033[1mЗакрываюсь!\033[0m")
     elif command.lower() == "del":
         notes_del()
     elif command.lower() == "edit":
         edit_notes()
     else:
-        print("You input not correct command...\n"
-              "To display all commands, use input: >>> help ")
+        print("Вы ввели не правильную команду...\n"
+              "Для справки и вывода основных команд используйте команду: >>> help ")
 
 
-""" This function saves the input
-     from user in database """
+""" Данная функция сохраняет ввод
+    от пользователя в базу данных """
 
 
 def add_notes():
-    title = str(input("\033[34mInput title: \033[0m"))
-    text = str(input("\033[34mIntup text note: \033[0m"))
-    com_orders = (title, text)
+    teg = str(input("\033[34mВведите тему: \033[0m"))
+    text = str(input("\033[34mВведите текст заметки: \033[0m"))
+    com_orders = (teg, text)
     cur.execute("INSERT INTO notes VALUES(?, ?);", com_orders)
     conn.commit()
-    print(f"Done! Title note - \033[32m{title}\033[0m saved!")
+    print(f"Готово! Заметка с именем - \033[32m{teg}\033[0m сохранена!")
 
 
-""" Returns a complete list of notes """
+""" Возвращает полный список названия заметок с их текством """
 
 
 def show_user_items():
@@ -61,53 +61,52 @@ def show_user_items():
         print(value)
 
 
-""" Returns a list of all available commands """
+""" Возвращает список всех доступных команд """
 
 
 def help_user():
     print("Command: \n"
-          "add - For adding new note\n"
-          "items - Returns a complete list of notes\n"
-          "del - For delete note\n"
-          "edit - For edit text\n"
-          "exit - For turn off app")
+          "add - Для добавления новой заметки\n"
+          "items - Для вывода всех заметок и их текста\n"
+          "del - Для удаления заметки\n"
+          "edit - Для редактирования текста заметки\n"
+          "exit - Хочу выйти")
 
 
-""" delete note """
+""" Функция удаления заметки """
 
 
 def notes_del():
-    title_item = str(input("\033[34mWhich note do you want to delete? \033[0m"))
-    if title_item == "not del":
-        print("\033[35mOkay, going back...\033[0m")
+    key_item = str(input("\033[34mКакую заметку вы хотите удалить? \033[0m"))
+    if key_item == "not del":
+        print("\033[35mХорошо, возвращаюсь обратно...\033[0m")
     else:
-        cur.execute(f"SELECT title FROM notes WHERE title = '{title_item}'")
+        cur.execute(f"SELECT teg FROM notes WHERE teg = '{key_item}'")
         if cur.fetchone() is None:
-            print("\033[31mYou don't have this note\033[0m")
+            print("\033[31mУ вас нет такой заметки\033[0m")
             notes_del()
         else:
-            cur.execute(f"DELETE FROM notes WHERE title = '{title_item}'")
+            cur.execute(f"DELETE FROM notes WHERE teg = '{key_item}'")
             conn.commit()
-            print(f"Note - \033[31m{title_item}\033[0m has been deleted")
+            print(f"Заметка - \033[31m{key_item}\033[0m была удалена")
 
 
-""" Note editing function """
+""" Функция редактирования заметки """
 
 
 def edit_notes():
-    notes_item = str(input("\033[34mWhich note do you want to edit? \033[0m"))
+    notes_item = str(input("\033[34mКакую заметку вы хотите редактировать? \033[0m"))
     if notes_item == "not edit":
-        print("\033[35mOkay, going back...\033[0m")
+        print("\033[35mХорошо, возвращаюсь обратно...\033[0m")
+    cur.execute(f"SELECT teg FROM notes WHERE teg = '{notes_item}'")
+    if cur.fetchone() is None:
+        print("\033[31mУ вас нет такой заметки\033[0m")
+        edit_notes()
     else:
-        cur.execute(f"SELECT title FROM notes WHERE title = '{notes_item}'")
-        if cur.fetchone() is None:
-            print("\033[31mYou don't have this note\033[0m")
-            edit_notes()
-        else:
-            edit_note = str(input("\033[34mInput text: \033[0m"))
-            cur.execute(f"UPDATE notes SET note = '{edit_note}' WHERE title = '{notes_item}'")
-            conn.commit()
-            print(f"Note \033[33m{notes_item}\033[0m has been edited")
+        edit_note = str(input("\033[34mВведите текст: \033[0m"))
+        cur.execute(f"UPDATE notes SET note = '{edit_note}' WHERE teg = '{notes_item}'")
+        conn.commit()
+        print(f"Заметка \033[33m{notes_item}\033[0m была изменена")
 
 x = True
 
---
+++ main_eng.py
@@ -0,0 +1,118 @@
+import sqlite3
+
+""" connecting to SQL """
+
+conn = sqlite3.connect("orders.db")
+cur = conn.cursor()
+
+""" creating database """
+
+cur.execute("""CREATE TABLE IF NOT EXISTS notes(
+    title TEXT,
+    note TEXT);
+""")
+conn.commit()
+
+
+def input_user():
+    global command
+    command = input("\033[34mPlease, input command: \033[0m")
+
+
+""" A function that returns the result of the command input to the user  """
+
+
+def command_user():
+    if command.lower() == "add":
+        add_notes()
+    elif command.lower() == "items":
+        show_user_items()
+    elif command.lower() == "help":
+        help_user()
+    elif command.lower() == "exit":
+        print("\033[36m\033[1mGoodBye!\033[0m")
+    elif command.lower() == "del":
+        notes_del()
+    elif command.lower() == "edit":
+        edit_notes()
+    else:
+        print("You input not correct command...\n"
+              "To display all commands, use input: >>> help ")
+
+
+""" This function saves the input
+     from user in database """
+
+
+def add_notes():
+    title = str(input("\033[34mInput title: \033[0m"))
+    text = str(input("\033[34mIntup text note: \033[0m"))
+    com_orders = (title, text)
+    cur.execute("INSERT INTO notes VALUES(?, ?);", com_orders)
+    conn.commit()
+    print(f"Done! Title note - \033[32m{title}\033[0m saved!")
+
+
+""" Returns a complete list of notes """
+
+
+def show_user_items():
+    for value in cur.execute("SELECT * FROM notes;"):
+        print(value)
+
+
+""" Returns a list of all available commands """
+
+
+def help_user():
+    print("Command: \n"
+          "add - For adding new note\n"
+          "items - Returns a complete list of notes\n"
+          "del - For delete note\n"
+          "edit - For edit text\n"
+          "exit - For turn off app")
+
+
+""" delete note """
+
+
+def notes_del():
+    title_item = str(input("\033[34mWhich note do you want to delete? \033[0m"))
+    if title_item == "not del":
+        print("\033[35mOkay, going back...\033[0m")
+    else:
+        cur.execute(f"SELECT title FROM notes WHERE title = '{title_item}'")
+        if cur.fetchone() is None:
+            print("\033[31mYou don't have this note\033[0m")
+            notes_del()
+        else:
+            cur.execute(f"DELETE FROM notes WHERE title = '{title_item}'")
+            conn.commit()
+            print(f"Note - \033[31m{title_item}\033[0m has been deleted")
+
+
+""" Note editing function """
+
+
+def edit_notes():
+    notes_item = str(input("\033[34mWhich note do you want to edit? \033[0m"))
+    if notes_item == "not edit":
+        print("\033[35mOkay, going back...\033[0m")
+    else:
+        cur.execute(f"SELECT title FROM notes WHERE title = '{notes_item}'")
+        if cur.fetchone() is None:
+            print("\033[31mYou don't have this note\033[0m")
+            edit_notes()
+        else:
+            edit_note = str(input("\033[34mInput text: \033[0m"))
+            cur.execute(f"UPDATE notes SET note = '{edit_note}' WHERE title = '{notes_item}'")
+            conn.commit()
+            print(f"Note \033[33m{notes_item}\033[0m has been edited")
+
+x = True
+
+while x:
+    input_user()
+    command_user()
+    if command == "exit":
+        break
